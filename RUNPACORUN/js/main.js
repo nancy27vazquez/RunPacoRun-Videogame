9ea0e2770a6d75ae6ba6e8d5f92dc5b9c0c1607f
nancy27vazquez <nevadg@outlook.com>
2019-08-22 21:17:31 -0500

    Printing Paco

--- RUNPACORUN/js/main.js
+++ RUNPACORUN/js/main.js
@@ -1,73 +1,91 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+//ctx.fillRect(0, 0, 50, 50);
+
 /* Generic variables*/
 let frames = 0;
 let gravity = 0.6;
-let interval = ;
+let interval = null;
 
 /* Audio & Effects */
-let audio = new Audio();
+/*let audio = new Audio();
 audio.loop = true;
-audio.src = 'link here';
+audio.src = "link here";
 
 let audioThrowWeapon = new Audio();
-audioThrowWeapon.src = 'link here';
+audioThrowWeapon.src = "link here";
 
 let audioDie = new Audio();
-audioDie.src = 'link here';
+audioDie.src = "link here";
 
 let audioWin = new Audio();
-audioWin.scr = 'link here';
-
+audioWin.scr = "link here";*/
 
 /* Classes */
-class Character {
-  constructor(x, y, width, height, image) {
+class Paco {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 130;
+    this.height = 218;
+    this.image = new Image();
+    this.image.src = "/img/pacoSprite/paco_normal.png";
+  }
+
+  draw() {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  }
+}
+
+const paco = new Paco(80, 200);
+
+/* Functions */
+function generateGame() {
+  frames++;
+  paco.draw();
+}
+
+function startGame() {
+  interval = setInterval(generateGame, 1000 / 60);
+}
+
+startGame();
+
+/*
+class Enemy {
+  constructor(x, y, width, height) {
     this.x = 0;
     this.y = 0;
     this.width = width;
     this.height = height;
     this.image = new Image();
-    this.image.src = 'https://vignette.wikia.nocookie.net/moj-wlasny-swiat/images/7/75/Mario.png/revision/latest?cb=20141208165919&path-prefix=pl';
+    this.image.src = "";
   }
 }
 
-class Paco extends Character{
-  constructor(x, y, width, height, life, weapons) {
-    super(x, y, width, height);
-    this.life = 10;
-    this.weapons = 20;
-  }
-}
-
-class Boss extends Character{
-  constructor(x, y, width, height, life, power){
-    super(x, y, width, height, life)
-    this.power = ;
+class Boss {
+  constructor(x, y, width, height) {
+    this.x = 0;
+    this.y = 0;
+    this.width = width;
+    this.height = height;
+    this.image = new Image();
+    this.image.src = "";
   }
 }
 
 class Background {
-  constructor(){
+  constructor() {
     this.x = 0;
     this.y = 0;
     this.width = canvas.width;
     this.height = canvas.height;
     this.image = new Image();
-    this.image.src = 'https://vignette.wikia.nocookie.net/moj-wlasny-swiat/images/7/75/Mario.png/revision/latest?cb=20141208165919&path-prefix=pl';
-  }
-  draw() {
-    this.x--;
-    if (this.x < -canvas.width) this.x = 0;
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-
-    ctx.drawImage(
-      this.image,
-      this.x + canvas.width,
-      this.y,
-      this.width,
-      this.height
-    );
+    this.image.src =
+      "https://vignette.wikia.nocookie.net/moj-wlasny-swiat/images/7/75/Mario.png/revision/latest?cb=20141208165919&path-prefix=pl";
   }
 }
+/* End Classes */
+
+/*======TESTING====== */
