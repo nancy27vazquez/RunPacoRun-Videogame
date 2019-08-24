494bd7bcda986f7fd732bb475bc70cabee1935c5
nancy27vazquez <nevadg@outlook.com>
2019-08-24 15:44:16 -0500

    Adding movement, knifes as enemies and game over

--- RUNPACORUN/js/main.js
+++ RUNPACORUN/js/main.js
@@ -1,18 +1,18 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
-//ctx.fillRect(0, 0, 50, 50);
-
 /* Generic variables*/
 let frames = 0;
 let gravity = 0.6;
-let interval = null;
+let enemies = [];
+let score = 0;
 
 /* Audio & Effects */
-/*let audio = new Audio();
+let audio = new Audio();
 audio.loop = true;
-audio.src = "link here";
-
+audio.src =
+  "https://ia600702.us.archive.org/25/items/FailRecorderMissionImpossibleThemesong/Fail%20Recorder_%20Mission%20Impossible%20Themesong.mp3";
+/*
 let audioThrowWeapon = new Audio();
 audioThrowWeapon.src = "link here";
 
@@ -45,8 +45,16 @@ class Paco {
     this.image4.src = "/img/pacoSprite/paco_run_right_1.png";
     this.imageRun = this.image1;*/
   }
-
+  collision(item) {
+    return (
+      this.x < item.x + item.width &&
+      this.x + this.width > item.x &&
+      this.y < item.y + item.height &&
+      this.y + this.height > item.y
+    );
+  }
   draw() {
+    if (this.y < 300) this.y += 6;
     if (frames % 10 === 0) {
       this.imageRun = this.imageRun == this.image1 ? this.image2 : this.image1;
     }
@@ -54,27 +62,26 @@ class Paco {
   }
 }
 
-const paco = new Paco(80, 400);
+const paco = new Paco(80, 300);
 
-/* Functions */
-setInterval(function() {
-  frames++;
-  ctx.clearRect(0, 0, 256, 256);
-  paco.draw();
-}, 1000 / 60);
-
-/*
 class Enemy {
-  constructor(x, y, width, height) {
-    this.x = 0;
-    this.y = 0;
-    this.width = width;
-    this.height = height;
+  constructor() {
+    this.x = canvas.width;
+    this.y = 400;
+    this.width = 70;
+    this.height = 70;
     this.image = new Image();
-    this.image.src = "";
+    this.image.src = "/img/knife.png";
+  }
+  draw() {
+    if (frames % 10) this.x -= 5;
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 }
 
+const enemy = new Enemy();
+
+/*
 class Boss {
   constructor(x, y, width, height) {
     this.x = 0;
@@ -85,7 +92,7 @@ class Boss {
     this.image.src = "";
   }
 }
-
+*/
 class Background {
   constructor() {
     this.x = 0;
@@ -94,9 +101,68 @@ class Background {
     this.height = canvas.height;
     this.image = new Image();
     this.image.src =
-      "https://vignette.wikia.nocookie.net/moj-wlasny-swiat/images/7/75/Mario.png/revision/latest?cb=20141208165919&path-prefix=pl";
+      "https://www.dondeir.com/wp-content/uploads/2018/01/tacos-baratos-7.jpg";
+  }
+  draw() {
+    this.x--;
+    if (this.x < -canvas.width) this.x = 0;
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.x + this.width,
+      this.y,
+      this.width,
+      this.height
+    );
+  }
+  gameOver() {
+    clearInterval(interval);
+    ctx.font = "100px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("perdiste paquito", 300, 370);
   }
 }
+
+const back = new Background(canvas.width, canvas.height);
+
 /* End Classes */
 
-/*======TESTING====== */
+/* Functions */
+let interval = setInterval(function() {
+  frames++;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  back.draw();
+  paco.draw();
+  generateEnemies();
+  drawEnemies();
+}, 1000 / 60);
+
+function generateEnemies() {
+  if (frames % 100 == 0 || frames % 60 == 0 || frames % 170 == 0) {
+    var enemy = new Enemy();
+    enemies.push(enemy);
+  }
+}
+
+function drawEnemies() {
+  enemies.forEach(function(enemy) {
+    enemy.draw();
+    if (paco.collision(enemy)) {
+      //console.log("Paco, te hicieron taco :(");
+      back.gameOver();
+    }
+  });
+}
+
+/* Events */
+addEventListener("keydown", function(event) {
+  if (event.keyCode === 38) {
+    paco.y -= 190;
+  }
+  if (event.keyCode === 39) {
+    paco.x += 15;
+  }
+  if (event.keyCode === 37) {
+    paco.x -= 15;
+  }
+});
