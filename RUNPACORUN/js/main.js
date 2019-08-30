b5e33c8ff634a3aeaae51d83e7a731cf4c0ed43d
nancy27vazquez <nevadg@outlook.com>
2019-08-29 22:11:25 -0500

    Adding an image when losing and randomly enemies on y

--- RUNPACORUN/js/main.js
+++ RUNPACORUN/js/main.js
@@ -6,12 +6,21 @@ let frames = 0;
 let gravity = 0.6;
 let enemies = [];
 let score = 0;
+let interval;
+let loser = new Image();
+loser.src = "/img/loser.png";
 
 /* Audio & Effects */
 let audio = new Audio();
 audio.loop = true;
 audio.src =
   "https://ia600702.us.archive.org/25/items/FailRecorderMissionImpossibleThemesong/Fail%20Recorder_%20Mission%20Impossible%20Themesong.mp3";
+
+let audioIntro = new Audio();
+audio.loop = true;
+audio.src =
+  "https://ia600702.us.archive.org/25/items/FailRecorderMissionImpossibleThemesong/Fail%20Recorder_%20Mission%20Impossible%20Themesong.mp3";
+
 /*
 let audioThrowWeapon = new Audio();
 audioThrowWeapon.src = "link here";
@@ -27,8 +36,8 @@ class Paco {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.width = 130;
-    this.height = 218;
+    this.width = 110;
+    this.height = 190;
     // Static
     /*this.image = new Image();
     this.image.src = "/img/pacoSprite/paco_normal.png";*/
@@ -65,16 +74,16 @@ class Paco {
 const paco = new Paco(80, 300);
 
 class Enemy {
-  constructor() {
+  constructor(y) {
     this.x = canvas.width;
-    this.y = 400;
+    this.y = y;
     this.width = 70;
     this.height = 70;
     this.image = new Image();
     this.image.src = "/img/knife.png";
   }
   draw() {
-    if (frames % 10) this.x -= 5;
+    if (frames % 20) this.x -= 5;
     ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 }
@@ -100,8 +109,7 @@ class Background {
     this.width = canvas.width;
     this.height = canvas.height;
     this.image = new Image();
-    this.image.src =
-      "/img/back.png";
+    this.image.src = "/img/back.png";
   }
   draw() {
     this.x--;
@@ -115,12 +123,6 @@ class Background {
       this.height
     );
   }
-  gameOver() {
-    clearInterval(interval);
-    ctx.font = "100px Arial";
-    ctx.fillStyle = "white";
-    ctx.fillText("perdiste paquito", 300, 370);
-  }
 }
 
 const back = new Background(canvas.width, canvas.height);
@@ -128,32 +130,56 @@ const back = new Background(canvas.width, canvas.height);
 /* End Classes */
 
 /* Functions */
-let interval = setInterval(function() {
-  frames++;
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  back.draw();
-  paco.draw();
-  generateEnemies();
-  drawEnemies();
-}, 1000 / 60);
-
+function start() {
+  interval = setInterval(function() {
+    frames++;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    back.draw();
+    paco.draw();
+    generateEnemies();
+    drawEnemies();
+  }, 1000 / 60);
+}
 function generateEnemies() {
   if (frames % 100 == 0 || frames % 60 == 0 || frames % 170 == 0) {
-    var enemy = new Enemy();
+    let randomPos = Math.floor(
+      Math.random() * (canvas.height - 70 - 175) + 175
+    );
+
+    var enemy = new Enemy(randomPos);
     enemies.push(enemy);
   }
 }
 
 function drawEnemies() {
   enemies.forEach(function(enemy) {
+    if (enemy.x + enemy.width < 0) {
+      enemies.splice(0, 1);
+    }
     enemy.draw();
     if (paco.collision(enemy)) {
       //console.log("Paco, te hicieron taco :(");
-      back.gameOver();
+      gameOver();
     }
   });
 }
+function gameOver() {
+  clearInterval(interval);
+  ctx.drawImage(loser, 200, 50, 500, 322);
+  ctx.font = "40px Arial";
+  ctx.fillStyle = "white";
+  ctx.fillText("Final score: xxxx", 310, 380);
+}
 
+function reset() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  paco.x = 30;
+  paco.y = 300;
+  audio.currentTime = 0;
+  enemies = [];
+  interval = undefined;
+  start();
+}
 /* Events */
 addEventListener("keydown", function(event) {
   if (event.keyCode === 38) {
@@ -165,4 +191,9 @@ addEventListener("keydown", function(event) {
   if (event.keyCode === 37) {
     paco.x -= 15;
   }
+  if (event.keyCode === 82) {
+    reset();
+    console.log("presionaste r");
+  }
 });
+start();
